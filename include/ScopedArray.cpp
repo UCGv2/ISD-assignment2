d5d80cf9ed5fb40331ab222e0bbb370115aadbdc
UCGv2 <chrisglennii@yahoo.com>
2018-09-12 22:11:29 -0500

    Everything should be good

--- include/ScopedArray.cpp
+++ include/ScopedArray.cpp
@@ -1,28 +1,76 @@
-//
-// Created by fours on 9/11/2018.
-//
+// File name: ScopedArray.cpp
+// Author: Christopher Glenn II
+// VUnetid: glennca1
+// Email: christopher.a.glenn@vanderbilt.edu
+// Class: CS3251
+// Date: 9/19/18
+// Honor statement: I have neither given nor received unauthorized help on this assignment
+// Assignment Number: 02
+// Description: This is an implementation of the class template ScopedArray. Below are
+// its function templates
 
 #include "ScopedArray.h"
-
+/**
+ * @brief This is the constructor for the ScopedArray class
+ * @param a is a pointer of type T and mArray will equal a.
+ * If the user does not specify what a is, it defaults to nullptr
+ */
 template <typename T>
 ScopedArray<T>::ScopedArray(T* a)
     : mArray(a)
 {
 }
-
+/**
+ * @brief Destructor for the ScopedArray class
+ */
 template <typename T> ScopedArray<T>::~ScopedArray()
 {
     delete[](mArray);
     mArray = nullptr;
 }
 
+/**
+ * @brief overloaded bool operator so ScopedArrays can be associated with a bool value
+ * @return whether mArray is empty or not
+ */
+template <typename T> ScopedArray<T>::operator bool() const noexcept
+{
+    return mArray != nullptr;
+}
+
+/**
+ * @brief overloaded[] const operator to allow the user to refer to the members
+ * of the array contained within mArray
+ * @param i array index
+ * @return a const reference to the T type object in mArray at index i
+ */
+template <typename T> const T& ScopedArray<T>::operator[](size_t i) const
+{
+    return mArray[i];
+}
+
+/**
+ * @brief overloaded[] operator to allow the user to refer to the members of
+ * the array contain within mArray in non-const ScopedArray objects
+ * @param i array index
+ * @return a reference to the T type object in mArray at index i
+ */
+template <typename T> T& ScopedArray<T>::operator[](size_t i)
+{
+    return mArray[i];
+}
+
+/**
+ * @brief Fetches mArray
+ * @return mArray
+ */
 template <typename T> T* ScopedArray<T>::get() const
 {
     return mArray;
 }
 /**
- * @brief Deletes the current array in mArray and replaces it with parameter "r"
- * @param r is a new array or nullptr
+ * @brief Deletes the current array in mArray and replaces it with parameter "next"
+ * @param next is a T pointer determined by the user, if they don't specify, it defaults to nullptr
  * @post mArray points to parameter "next"
  */
 template <typename T> void ScopedArray<T>::reset(T* next)
@@ -33,10 +81,10 @@ template <typename T> void ScopedArray<T>::reset(T* next)
     }
 }
 /**
- * @brief Swaps the mArray fields of two different SimpleArrays
- * @details Swaps the mArray fields of two SimpleArrays provided their mArray fields
+ * @brief Swaps the mArray fields of two different ScopedArrays
+ * @details Swaps the mArray fields of two ScopedArrays provided their mArray fields
  * are pointing to different locations. If they are the same, nothing needs to happen.
- * @param a2 is a different SimpleArray
+ * @param a2 is a different ScopedArray
  */
 template <typename T> void ScopedArray<T>::swap(ScopedArray<T>& a2)
 {
--- include/ScopedArray.h
+++ include/ScopedArray.h
@@ -1,40 +1,70 @@
-//
-// Created by fours on 9/11/2018.
-//
+// File name: SimpleArray.h
+// Author: Christopher Glenn II
+// VUnetid: glennca1
+// Email: christopher.a.glenn@vanderbilt.edu
+// Class: CS3251
+// Date: 9/19/18
+// Honor statement: I have neither given nor received unauthorized help on this assignment
+// Assignment Number: 02
+// Description: This is a declaration of the class template ScopedArray
 
 #ifndef ASSIGNMENT2_SOLUTION_SCOPEDARRAY_H
 #define ASSIGNMENT2_SOLUTION_SCOPEDARRAY_H
 
 #include "AllocationTracker.h"
 /**
- * @brief Simulates a simplified array structure
- * @details Simulates the referential nature of arrays by pointing an AllocationTracker* to an
- * array of AllocationTracker objects. SimpleArrays can be constructed but not assigned or copied.
+ * @brief This is yet another simplified array structure, but this time it is implemented
+ * using templates, so the array can hold more than just AllocationTracker objects
+ * @tparam T this is the type of item the array will hold as defined by the user
  */
 template <typename T> class ScopedArray {
 public:
     /*** Add some constructors/destructor goodness here ***/
 
     /**
-     * @brief Constructor for the ScopedArray class
+     * @brief This is the constructor for the ScopedArray class
+     * @param a is a pointer of type T and mArray will equal a.
+     * If the user does not specify what a is, it defaults to nullptr
      */
     explicit ScopedArray(T* a = nullptr);
 
     /**
-     *
-     * @param noCopy
+     * @brief This is the deleted Copy Constructor
+     * @param noCopy nothing can be copied
      */
     ScopedArray(const ScopedArray<T>& noCopy) = delete;
 
     /**
-     * @brief Destructor for the Simple Array class
+     * @brief Destructor for the ScopedArray class
      */
     ~ScopedArray();
 
     /**
-     *
-     * @param noAssign
-     * @return
+     * @brief overloaded bool operator so ScopedArrays can be associated with a bool value
+     * @return whether mArray is empty or not
+     */
+    operator bool() const noexcept;
+
+    /**
+     * @brief overloaded[] const operator to allow the user to refer to the members
+     * of the array contained within mArray
+     * @param i array index
+     * @return a const reference to the T type object in mArray at index i
+     */
+    const T& operator[](size_t i) const;
+
+    /**
+     * @brief overloaded[] operator to allow the user to refer to the members of
+     * the array contain within mArray in non-const ScopedArray objects
+     * @param i array index
+     * @return a reference to the T type object in mArray at index i
+     */
+    T& operator[](size_t i);
+
+    /**
+     * the deleted assignment operator
+     * @param noAssign cannot assign anything
+     * @return can't return anything
      */
     ScopedArray<T>& operator=(const ScopedArray<T>& noAssign) = delete;
 
@@ -46,30 +76,26 @@ public:
      */
     T* get() const;
     /**
-     * @brief Deletes the current array in mArray and replaces it with parameter "r"
-     * @param r is a new array or nullptr
-     * @post mArray points to parameter "r"
+     * @brief Deletes the current array in mArray and replaces it with parameter "next"
+     * @param next is a T pointer determined by the user, if they don't specify, it defaults to
+     * nullptr
+     * @post mArray points to parameter "next"
      */
     void reset(T* next = nullptr);
     /**
-     * @brief Swaps the mArray fields of two different SimpleArrays
-     * @details Swaps the mArray fields of two SimpleArrays provided their mArray fields
+     * @brief Swaps the mArray fields of two different ScopedArrays
+     * @details Swaps the mArray fields of two ScopedArrays provided their mArray fields
      * are pointing to different locations. If they are the same, nothing needs to happen.
-     * @param a2 is a different SimpleArray
+     * @param a2 is a different ScopedArray
      */
     void swap(ScopedArray<T>& a2);
 
     /**
-     * @brief Returns the AllocationTracker* associated with mArray and mArray
+     * @brief Returns the T* associated with mArray and mArray
      * is pointed to nullptr
      * @return Returns the former address that mArray referenced
      */
     T* release();
-    /**
-     * @brief returns whether or not mArray equals nullptr
-     * @return the evaluation of the equivalence between mArray and nullptr
-     */
-    bool isNonNull() const;
 
 private:
     /*** Maybe some things here too ***/
